e3e48370af8aa17e987b2a7944312480da52db85
ivosth <43857495+ivosth@users.noreply.github.com>
2019-12-07 17:20:39 +0100

    Update main.cc

--- implementacion/main.cc
+++ implementacion/main.cc
@@ -13,12 +13,20 @@ int main(void)
   {
     cout<<"1.Mostrar paciente"<<endl;
     cout<<"2.Añadir paciente"<<endl;
-    cout<<"3.Cambiar paciente"<<endl;
+    cout<<"3.Modificar paciente"<<endl;
     cout<<"4.Eliminar paciente"<<endl;
-    cout<<"5.Añadir cita"<<endl;
-    cout<<"6.Cambiar cita"<<endl;
-    cout<<"14.Añadir entrada del historial"<<endl;
-    cout<<"15.Mostrar historial"<<endl;
+    cout<<"5.Mostrar cita diarias"<<endl;
+    cout<<"6.Mostrar citas"<<endl;
+    cout<<"7.Añadir cita"<<endl;
+    cout<<"8.Modificar cita"<<endl;
+    cout<<"9.Cancelar cita"<<endl;
+    cout<<"10.Añadir tratamiento"<<endl;
+    cout<<"11.Modificar tratamiento"<<endl;
+    cout<<"12.Mostrar tratamiento"<<endl;
+    cout<<"13.Añadir entrada del historial"<<endl;
+    cout<<"14.Mostrar historial"<<endl;
+    cout<<"15.Salir"<<endl;
+
     cin>>option;
     getline(cin, buffer);
 
@@ -27,19 +35,19 @@ int main(void)
       case 1:
       {
         if(show_patient_menu()==false)
-          cout<<"Error"<<endl;
+          cout<<"Error al mostrar el paciente"<<endl;
       }break;
 
       case 2:
       {
         if(add_patient_menu()==false)
-          cout<<"Error"<<endl;
+          cout<<"No se ha añadido al paciente"<<endl;
       }break;
 
       case 3:
       {
         if(change_patient_menu()==false)
-          cout<<"Error"<<endl;
+          cout<<"No se ha modificado el paciente"<<endl;
       }break;
 
       case 4:
@@ -50,30 +58,66 @@ int main(void)
 
       case 5:
       {
+        if(todays_dates()==false)
+          cout<<"No hay citas programadas para hoy"<<endl;
+      }break;
+
+      case 6:
+      {
+        if(true)
+          cout<<"Error"<<endl;
+      }break;
+
+      case 7:
+      {
         if(add_date_menu()==false)
           cout<<"La cita no se ha añadido"<<endl;
       }break;
 
-      case 6:
+      case 8:
       {
         if(update_date_menu()==false)
           cout<<"La cita no se ha modificado"<<endl;
       }break;
 
-      case 14:
+      case 9:
+      {
+        if(true)
+          cout<<"Error"<<endl;
+      }break;
+
+      case 10:
+      {
+        if(true)
+          cout<<"Error"<<endl;
+      }break;
+
+      case 11:
+      {
+        if(true)
+          cout<<"Error"<<endl;
+      }break;
+
+      case 12:
+      {
+        if(true)
+          cout<<"Error"<<endl;
+      }break;
+
+      case 13:
       {
         if(add_history_entry_menu()==false)
           cout<<"No se ha añadido la entrada al historial"<<endl;
       }break;
 
-      case 15:
+      case 14:
       {
         if(show_history_menu()==false)
-          cout<<"Error"<<endl;
+          cout<<"No se ha podido mostrar el historial del paciente"<<endl;
       }break;
     }
 
-  } while(option!=16);
+  } while(option!=15);
 
 
 
