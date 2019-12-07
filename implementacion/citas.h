a14d869a319a137df9cf54e71d95b3ba63c3ed1a
ivosth <43857495+ivosth@users.noreply.github.com>
2019-12-07 17:17:51 +0100

    Update citas.h

--- implementacion/citas.h
+++ implementacion/citas.h
@@ -16,6 +16,7 @@ class Date
     Date(string fullname, string date="", string start="", string end="", string description="");
     void add_date();
     void update_date(int n);
+    bool todays_dates(string date);   
     //void delete_date_menu();
 };
 
