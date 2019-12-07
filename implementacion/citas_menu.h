e6ce88dcdcaa35889e85c7e3e789b9c32731a6ef
ivosth <43857495+ivosth@users.noreply.github.com>
2019-12-07 17:17:31 +0100

    Update citas_menu.h

--- implementacion/citas_menu.h
+++ implementacion/citas_menu.h
@@ -3,10 +3,10 @@
 #include <string>
 using namespace std;
 
+string check_date(string ini, string end);
 bool add_date_menu();
 bool update_date_menu();
-string check_date(string ini, string end);
-
+bool todays_dates();
 //void delete_date_menu();
 
 #endif
