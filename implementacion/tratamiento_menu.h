488e58e95e79819869b17d30c5dc8a0ac506467f
Nel <43857494+spookynel@users.noreply.github.com>
2019-12-16 19:07:37 +0100

    Update tratamiento_menu.h

--- implementacion/tratamiento_menu.h
+++ implementacion/tratamiento_menu.h
@@ -1,8 +1,10 @@
 #ifndef TRATAMIENTO_MENU_H
 #define TRATAMIENTO_MENU_H
-#include <string>
+
 using namespace std;
 
-
+bool add_treatment_menu();
+bool show_treatment_menu();
+bool modify_treatment_menu();
 
 #endif
