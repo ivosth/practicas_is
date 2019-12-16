739531a84bd452a54eec090dad3bfed644dcb62c
Nel <43857494+spookynel@users.noreply.github.com>
2019-12-16 19:06:30 +0100

    Update tratamiento.h

--- implementacion/tratamiento.h
+++ implementacion/tratamiento.h
@@ -2,9 +2,33 @@
 #define TRATAMIENTO_H
 
 #include <string>
+#include <list>
+
 using namespace std;
 
+class Treatment{
+private:
+  string fullname_;
+  string start_;
+  string end_;
+  string regularity_;
+  string concentration_;
+  string dose_;
+public:
+  Treatment(string fullname, string start="", string end="", string regularity="", string concentration="", string dose="");
+  inline string getStart() const {return start_;};
+  inline string getEnd() const {return end_;};
+  inline string getRegularity() const {return regularity_;};
+  inline string getConcentration() const {return concentration_;};
+  inline string getDose() const {return dose_;};
+  void add_treatment();
+  void show_treatment(list<Treatment> t);
+  void modify_treatment(int);
 
+
+
+
+};
 
 
 
