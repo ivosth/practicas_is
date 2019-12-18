50477d374b921458980d969cd92ffd5506719581
i82ruorj <55441382+i82ruorj@users.noreply.github.com>
2019-12-18 16:05:11 +0100

    Update citas_menu.h

--- implementacion/citas_menu.h
+++ implementacion/citas_menu.h
@@ -7,6 +7,7 @@ string check_date(string ini, string end);
 bool add_date_menu();
 bool update_date_menu();
 bool todays_dates();
-//void delete_date_menu();
+bool delete_date_menu();
+bool show_dates_menu();
 
 #endif
